51f582ba59516bda2e7164237aae0df3a0409c15
mambax7 <mambax7@gmail.com>
2019-12-31 17:37:42 -0500

    tinymce4

---
+++ htdocs/class/xoopseditor/tinymce4/include/xoopsquote.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *  TinyMCE adapter for XOOPS
+ *
+ * @copyright       XOOPS Project (https://xoops.org)
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package         class / xoopseditor
+ * @subpackage      tinymce
+ * @since           2.3.0
+ * @author          Laurent JEN <dugris@frxoops.org>
+ * @version         $Id: xoopsquote.php 8066 2011-11-06 05:09:33Z beckmi $
+ */
+if (!defined("XOOPS_ROOT_PATH")) { die("XOOPS root path not defined"); }
+// Add your code here to check acces by groups
+return true;
