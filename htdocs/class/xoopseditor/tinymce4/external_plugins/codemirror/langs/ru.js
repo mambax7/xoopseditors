29828ab87bc96455839a9935d7fc83f77590c73b
mambax7 <mambax7@gmail.com>
2019-12-31 17:32:48 -0500

    bootstrap plugin

--- htdocs/class/xoopseditor/tinymce4/external_plugins/codemirror/langs/ru.js
+++
@@ -1,8 +0,0 @@
-tinymce.addI18n('ru',{
-    'HTML source code': 'Исходный код HTML',
-    'Start search': 'Приступить к поиску',
-    'Find next': 'Искать далее',
-    'Find previous': 'Найти предыдущий',
-    'Replace': 'Заменить',
-    'Replace all': 'Заменить все'
-});
