a7a4aa2bbc1c3ff794add442d46387ee5e927b9b
mambax7 <mambax7@gmail.com>
2019-12-31 19:23:32 -0500

    fix https

--- htdocs/class/xoopseditor/tinymce4bootstrap/external_plugins/filemanager/dialog.php
+++ htdocs/class/xoopseditor/tinymce4bootstrap/external_plugins/filemanager/dialog.php
@@ -129,7 +129,7 @@ $get_params = http_build_query(
 ?>
 
 <!DOCTYPE html>
-<html xmlns="https://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
